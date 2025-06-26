6ec6c9d325fff04724a8f8cb33994323e69b3cea
marco0Fonseca <139077237+marco0Fonseca@users.noreply.github.com>
2025-06-26 13:24:01 +0000

    Modify exec.c

--- minix/servers/pm/exec.c
+++ minix/servers/pm/exec.c
@@ -51,9 +51,7 @@ do_exec(void)
 
 	tell_vfs(mp, &m);
 
-	if (m_in.m_lc_pm_exec.name != NULL) {
-    	printf("Executando: %s\n", (char *)m_in.m_lc_pm_exec.name);
-	}
+	printf("Executando: %*s\n", m_in.m_lc_pm_exec.namelen ,(char *)m_in.m_lc_pm_exec.name);
 
 	/* Do not reply */
 	return SUSPEND;
